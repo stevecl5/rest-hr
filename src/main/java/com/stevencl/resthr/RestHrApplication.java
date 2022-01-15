26b7f63685e73cb06a0d614aea5d2b45ac272cd7
stevecl5 <steveleighton5@gmail.com>
2022-01-15 13:35:29 -0700

    initial commit

---
+++ src/main/java/com/stevencl/resthr/RestHrApplication.java
@@ -0,0 +1,13 @@
+package com.stevencl.resthr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stevencl/resthr/RestHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevencl.resthr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
